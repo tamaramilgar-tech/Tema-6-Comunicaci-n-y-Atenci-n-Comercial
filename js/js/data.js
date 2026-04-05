002a1d752b9182587079091bccf30a453ff78be8
tamaramilgar-tech <tamara.milgar@gmail.com>
2026-04-06 00:08:46 +0100

    Create data.js

---
+++ js/js/data.js
@@ -0,0 +1,11 @@
+window.UNIT_NAME = "Unidad 6 · Comunicación y atención comercial";
+
+window.phase1Bank = [
+{ q:"Primera fase del proceso de compra", options:["Compra","Necesidad","Evaluación"], answerIndex:1 },
+{ q:"Kotler estudia...", options:["Producción","Marketing","Finanzas"], answerIndex:1 }
+];
+
+window.phase2Bank = window.phase1Bank;
+window.phase3Bank = window.phase1Bank;
+window.phase4Bank = window.phase1Bank;
+window.phase5Bank = window.phase1Bank;
